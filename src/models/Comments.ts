e5c81b3b567bba4023e2dfc5b74169d7fb2492fc
a.cherkharov <0PHHbE@|Z>
2024-10-03 18:50:37 +0300

    бд + решение домашнего задания

---
+++ src/models/Comments.ts
@@ -0,0 +1,17 @@
+import mongoose, { Date, Document, Schema } from "mongoose";
+
+export interface IComment extends Document {
+    postId: mongoose.Schema.Types.ObjectId;
+    authorId: mongoose.Schema.Types.ObjectId;
+    text: string;
+    createdAt: Date;
+}
+
+const CommentSchema: Schema = new Schema({
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    authorid: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+})
+
+export default mongoose.model<IComment>('Comment', CommentSchema) 
